97a52f354373ce2ef4e3c9257b9a3a47ff8ebaf2
lnagler1 <lnagler1@htl-steyr.ac.at>
2022-01-20 16:42:58 +0100

    Da Reschta

--- mrester.java
+++ mrester.java
@@ -1,6 +1,6 @@
 public class reschta {
 
     public static void main(String[] args) {
-        System.out.print("Da Reschtaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa oasch!");
+        System.out.print("Da Reschtaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa is nimma do s bled!");
     }
 }
