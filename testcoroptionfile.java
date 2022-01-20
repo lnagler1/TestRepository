21075d7e258fdaf5cb939a604c33762d29255dfb
lnagler1 <lnagler1@htl-steyr.ac.at>
2022-01-20 16:27:40 +0100

    Test

--- testcoroptionfile.java
+++ testcoroptionfile.java
@@ -2,3 +2,12 @@
 //Seas
 //Hallo von Lukas
 //ddfljsflkgslfkgjs
+
+
+
+public class testcoroptionfile {
+    public static void main(String[] args) {
+        //Hallo
+        System.out.println("Ich bin Lukas");
+    }
+}
