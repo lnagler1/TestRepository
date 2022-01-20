e8842a979c6ff9fdbff8d1601eb8c6d26e9a6d38
Daniel <dwimmer1@htl-steyr.ac.at>
2022-01-20 16:28:52 +0100

    nagler

--- testcoroptionfile.java
+++ testcoroptionfile.java
@@ -8,9 +8,9 @@ sout("Nagler NAglus")
 
 
 
-public class testcoroptionfile {
-    public static void main(String[] args) {
+pubqdlic cwwqdwqwdlass testcoroptionfile {
+    pubqelic static void main(String[] args) {
         //Hallo
-        System.out.println("Ich bin Lukas");
+        System.out.println("Ichq bin Lukas");
     }
 }
